15cc5bb1b66d1331636db8e222ab0bd651df5e20
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-03-24 20:10:21 +0900

    Removed error throwing and implemented skipping stringification of functions/undefined values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -19,13 +19,13 @@ var stringifyJSON = function (obj) {
     result = result.concat('{');
 
     for (var i = 0; i < keys.length; i++) {
-      if (i) {
-      	result = result.concat(',');
-      }
+    	if (starter(keys[i]) !== '' && starter(token[keys[i]]) !== '') {
+        if (i) {
+      	  result = result.concat(',');
+        }
 
-      result = result.concat(starter(keys[i]));
-      result = result.concat(':');
-      result = result.concat(starter(token[keys[i]]));
+        result = result.concat(starter(keys[i]) + ':' + starter(token[keys[i]]));
+    	}
     }
 
     result = result.concat('}');
@@ -37,11 +37,13 @@ var stringifyJSON = function (obj) {
     var result = '[';
 
     for (var i = 0; i < token.length; i++) {
-    	if (i) {
-    		result = result.concat(',');
-    	}
+    	if (starter(token[i]) !== '') {
+    	  if (i) {
+    		  result = result.concat(',');
+    	  }
 
-    	result = result.concat(starter(token[i]));
+    	  result = result.concat(starter(token[i]));
+    	}
     }
 
     return result.concat(']');;
@@ -68,7 +70,8 @@ var stringifyJSON = function (obj) {
 
   var starter = function(token) {
     if (typeof stringifyActions[typeof token] !== 'function') {
-    	throw new Error('Invalid JSON object.');
+    	//throw new Error('Invalid JSON object.');
+    	return '';
     }
 
     return stringifyActions[typeof token](token);
