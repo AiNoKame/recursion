2e01513edf726dd2c317321f8ecb67fe6cecce5e
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-03-24 17:56:19 +0900

    Outlined handlers and implemented recursive function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,50 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var  result = '';
+
+  var handleObject = function(token) {
+    if (token === null) {
+    	result = result.concat('null');
+    }
+
+    if (token instanceof Array) {
+    	handleArray(token);
+    }
+  };
+
+  var handleArray = function(token) {
+
+  };
+
+  var handleString = function(token) {
+
+  };
+
+  var handleBoolean = function(token) {
+
+  };
+
+  var handleNumber = function(token) {
+
+  };
+
+  var stringifyActions = {
+    'object': handleObject,
+    'string': handleString,
+    'boolean': handleBoolean,
+    'number': handleNumber
+  };
+
+  var starter = function(token) {
+    if (typeof stringifyActions[typeof token] !== 'function') {
+    	throw new Error('Invalid JSON object.');
+    }
+
+    stringifyActions[typeof token](token);
+  };
+
+  starter(obj);
+
+  return result;
 };
